dd79802e071dcad615fd38d56ef8d20128c93a1b
Tom Hosker <tomdothosker@gmail.com>
2026-06-07 00:52:01 +0100

    [codex] keep CI helper in package

--- source/__init__.py
+++ source/__init__.py
@@ -5,6 +5,7 @@ through the modules in which they are defined.
 """
 
 # Local imports.
+from .continuous_integration import run_continuous_integration
 from .hm_software_installer import HMSoftwareInstaller, install_hmss
 from .install_dependencies import (
     install_dependency,
